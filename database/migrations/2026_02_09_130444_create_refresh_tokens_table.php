1ad4ab11c4807e617a88e3667e01c9abb39c4b76
psychopro366 <psychopro366@gmail.com>
2026-02-09 19:05:56 +0530

    First Commit

---
+++ database/migrations/2026_02_09_130444_create_refresh_tokens_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('refresh_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('refresh_token', 500)->unique();
+            $table->string('device_id')->unique();
+            $table->string('device_info', 500)->nullable();
+            $table->timestamp('expired_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('refresh_tokens');
+    }
+};
